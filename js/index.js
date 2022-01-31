c707f784afbcb25c44fd6852e5007b328c0c2f18
Sandy <sandy.congreve@cmeducations.se>
2022-01-31 21:29:25 +0100

    new files

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const displayedPhotos = document.querySelector(".image-grid");
 const pageIndicator = document.querySelector(".page-indicator");
 const disabled = document.querySelector(".disabled");
+const loader = document.querySelector("#loading")
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
 
 const settings = {
   "async": true,
@@ -14,8 +17,7 @@ const settings = {
   "limit": 12,
   "order": "asc",
 }
-const previous = document.querySelector(".previous");
-const next = document.querySelector(".next");
+
 
 function renderNewPhotos(data) {
   displayedPhotos.textContent = null;
@@ -27,6 +29,15 @@ function renderNewPhotos(data) {
   });
 }
 
+function displayLoading() {
+    loader.classList.add("display");
+
+}
+function hideLoading() {
+    loader.classList.remove("display");
+}
+
+
 async function getCats() {
   const url = new URL(settings.url);
 
@@ -34,8 +45,12 @@ async function getCats() {
   url.searchParams.append("page", settings.page);
   url.searchParams.append("order", settings.order);
 
-  pageIndicator.textContent = `Showing page ${settings.page}`;
-  displayedPhotos.textContent = "Loading..";
+  pageIndicator.textContent = `Page ${settings.page}`;
+  displayedPhotos.textContent = "Loading new photos..";
+  displayLoading();
+ 
+
+
 
   toggleButtons(true);
 
@@ -47,12 +62,14 @@ async function getCats() {
     });
     const data = await response.json();
     renderNewPhotos(data);
+    
   } catch (err) {
     displayedPhotos.textContent =
       "Something went wrong while fetching data from the server";
   } finally {
     toggleButtons(false);
     previous.disabled = settings.page === 0;
+    hideLoading();
   }
 }
 
